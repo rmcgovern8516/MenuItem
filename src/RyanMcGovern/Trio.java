6d0e443382b29adf95ef913d6e9859a75b5063a8
BT_1N3_32 <rmcgovern8516@bths.edu>
2018-11-26 08:41:58 -0500

    v2

--- src/RyanMcGovern/Trio.java
+++ src/RyanMcGovern/Trio.java
@@ -1,4 +1,16 @@
 package RyanMcGovern;
 
 public class Trio {
+    private Sandwich sandwich;
+    private Salad salad;
+    private Drink drink;
+
+    public Trio(Sandwich sandwich, Salad salad, Drink drink){
+        this.sandwich = sandwich;
+        this.salad = salad;
+        this.drink = drink;
+    }
+
+    public String getName(String sandwich, String salad, String drink){
+    }
 }
